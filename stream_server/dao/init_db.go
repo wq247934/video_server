a4d7892da5678fbc80b18d036d5e5e5c59430ee0
wangqian <wq247934918@gmail.com>
2022-05-15 12:15:22 +0800

    feature:init create admin user

--- stream_server/dao/init_db.go
+++ stream_server/dao/init_db.go
@@ -1,6 +1,7 @@
 package dao
 
 import (
+	"gorm.io/gorm"
 	"log"
 	"video_server/stream_server/common"
 )
@@ -16,4 +17,28 @@ func init() {
 		panic(err)
 	}
 	log.Println("Migrate DB successfully!")
+	initUser()
+}
+
+func initUser() {
+	user := User{
+		Username: "admin",
+	}
+	if user.Find().Username == "" {
+		log.Println("init user")
+		u := User{
+			Model:    gorm.Model{},
+			Username: "admin",
+			Nickname: "admin",
+			Salt:     "diisadsytw70un02gh8822a",
+			Password: "2371686ba2742845c7ca628e90c84078d60bd51d52a571c7f174ecfbcddf6095", // password : 123456
+		}
+		err := u.Save()
+		if err != nil {
+			log.Println("init user failed", err)
+			return
+		}
+		return
+	}
+	log.Println("user admin is exists!")
 }
